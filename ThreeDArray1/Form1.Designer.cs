f5c356f162cd689128c18aa515cdc163247850fc
mr-subroutine <darrylshack@outlook.com>
2020-05-20 15:24:17 -0700

    added button functionality

--- ThreeDArray1/Form1.Designer.cs
+++ ThreeDArray1/Form1.Designer.cs
@@ -31,12 +31,11 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(471, 382);
@@ -44,42 +43,37 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 416);
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(371, 417);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(112, 59);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "TEST: Double Data for May 2020";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(198, 416);
+            this.button2.Location = new System.Drawing.Point(13, 417);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(112, 59);
             this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
+            this.button2.Text = "Display 3D Array";
             this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(371, 416);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(112, 59);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(495, 504);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "COVID-19 Positive Results King County WA";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -90,7 +84,6 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
     }
 }
 
--- ThreeDArray1/Form1.cs
+++ ThreeDArray1/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace ThreeDArray1
 {
@@ -15,6 +17,114 @@ namespace ThreeDArray1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        int[,,] covidPositiveData = new int[1, 7, 2];
+        int[,,] covidPositiveDataDiff = new int[1, 7, 2];
+        string[] days = new string[7];
+        string[] months = new string[2];
+        bool doubleData = false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string StartUpPath = Application.StartupPath;
+            string fileLocation = StartUpPath + @"\covid_data.txt";
+
+            string[] fileData = { "8", "8", "11", "14", "8", "13", "19", "214",
+                "187", "186", "177", "148", "122", "112", "1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "April", "May" };
+
+            File.WriteAllLines(@fileLocation, fileData);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            string StartUpPath = Application.StartupPath;
+            string fileLocation = StartUpPath + @"\covid_data.txt";
+
+            // Add Data to 3D Array
+            using (StreamReader read = new StreamReader(fileLocation))
+            {
+                for (int slice = 0; slice <= 1; slice++)
+                {
+                    for (int row = 0; row <= 0; row++)
+                    {
+                        for (int col = 0; col < 7; col++)
+                        {
+                            covidPositiveData[row, col, slice] = Convert.ToInt32(read.ReadLine());
+                        }
+                    }
+                }
+
+                // Add Days to 2D Array
+                for (int i = 0; i <= 6; i++)
+                {
+                    days[i] = read.ReadLine();
+                }
+
+                // Add Months to Array
+                for (int i = 0; i <= 1; i++)
+                {
+                    months[i] = read.ReadLine();
+                }
+                read.Close();
+
+                displayBasicData();
+                button1.Enabled = true;
+            }
+        }
+
+        private void displayBasicData(bool doubling = false)
+        {
+            textBox1.Clear();
+            if (doubling == false)
+            {
+                textBox1.Text += "King County COVID-19 Confirmed Cases for 1st Week of Month [2020]";
+            }
+            else
+            {
+                textBox1.Text += "King County COVID-19 Confirmed Cases for 1st Week of Month [2020] DOUBLED";
+            }
+
+            for (int slice = 0; slice <= 1; slice++)
+            {
+                int counter = 0;
+                textBox1.Text += Environment.NewLine;
+                textBox1.Text += Environment.NewLine;
+                textBox1.Text += months[slice] + "\t";
+
+                do
+                {
+                    textBox1.Text += days[counter] + "\t";
+                    counter++;
+                } while (counter != 7);
+
+                for (int row = 0; row <= 0; row++)
+                {
+                    textBox1.Text += Environment.NewLine;
+                    textBox1.Text += "\t";
+                    for (int col = 0; col < 7; col++)
+                    {
+                        if (doubleData == false)
+                        {
+                            textBox1.Text += covidPositiveData[row, col, slice].ToString() + "\t";
+                        }
+                        else if (doubleData == true)
+                        {
+                            int value = covidPositiveData[row, col, slice] * 2;
+                            textBox1.Text += value.ToString() + "\t";
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            doubleData = true;
+            displayBasicData(doubleData);
+            doubleData = false;
         }
     }
 }
